1a9a8874da352495b4629782527faeee32982126
Est3banR <eramirezmo100@gmail.com>
2026-04-12 15:21:28 -0600

    Comments added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,1 @@
-"""Modulo reservado para la interfaz.
-
-La logica de prediccion que debe consumir la persona encargada del frontend es:
-
-- `model.predict.predict_patient(...)`
-- `model.predict.predict(input_data)`
-- `model.predict.build_model_input(input_data)`
-- `model.predict.load_model()`
-"""
+# Hello
